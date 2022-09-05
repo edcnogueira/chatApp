4ddc4b79220c89c57aabe5da11ef2f8ff32e6d26
Edjan <edjan.nogueira14@gmail.com>
2022-09-05 19:34:39 -0300

    feat: cria server da aplicação.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,30 @@
 const express = require("express")
+const {createServer} = require("http")
+const {Server} = require("socket.io")
 
 const app = express()
+const server = createServer(app)
+const port = 3333
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
+
+io.on("connection", socket => {
+    console.log(`a user connected: ${socket.id}`)
+
+    socket.on("join_room", ({user, room}) => {
+        socket.join(room)
+    })
+
+    socket.on("send_msg", ( {room, user, message} ) => {
+        const d = {user: user, message: message}
+        socket.to(room).emit("recived_msg", d)
+    })
+})
 
 app.use(express.json())
 
-app.listen(3333, () => console.log("Server running in port 3333"))
+server.listen(port, () => console.log(`Server running in port ${port}`))
